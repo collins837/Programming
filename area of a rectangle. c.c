36201f7d8fc900e753d754a143ff9a3e0fc697d5
collins837 <cmutinda888@gmail.com>
2025-02-24 17:37:53 +0300

    Update area of a rectangle. c.c

--- area of a rectangle. c.c
+++ area of a rectangle. c.c
@@ -1,12 +1,14 @@
+//program to calculate area of a rectangle
+// the user is required to input:length and width
 #include<stdio.h>
 int main ( ) {
   int length=0;
   int width=0;
   printf("length:");
-  scanf("%d", &length);
+  scanf("%d", &length);//input length
   printf("width:");
-  scanf("%d", &width);
+  scanf("%d", &width);//input width
   int area=length*width;
-  printf("area:%d\n", area);
+  printf("area:%d\n", area);//output area according to formula A,=L*W
   return 0;
 }
